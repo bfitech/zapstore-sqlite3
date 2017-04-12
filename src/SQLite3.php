e24d48030c9ecaa39c1762ac9c3a4ab377a7e245
Rendra Suroso <rs@cogsci.bandungfe.net>
2017-04-12 17:07:29 +0700

    Tweak build configuration.

--- src/SQLite3.php
+++ src/SQLite3.php
@@ -6,6 +6,14 @@ namespace BFITech\ZapStore;
 
 class SQLite3 extends SQL {
 
+	/**
+	 * Constructor.
+	 *
+	 * @param array $params SQL connection dict exactly the same with
+	 *     that in the parent class except that 'dbtype' key can be
+	 *     omitted.
+	 * @param Logger $logger Instance of BFITech\\ZapCore\\Logger.
+	 */
 	public function __construct($params, $logger=null) {
 		$params['dbtype'] = 'sqlite3';
 		parent::__construct($params, $logger);
