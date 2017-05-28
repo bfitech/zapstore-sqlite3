ac2b3d1b1626e0b4c07e0493c3fd2fc705087cbd
Rendra Suroso <rs@cogsci.bandungfe.net>
2017-05-28 07:54:28 +0700

    Create true metapackage.

--- tests/SQLite3Test.php
+++ tests/SQLite3Test.php
@@ -1,19 +1,5 @@
 <?php
 
 
-use BFITech\ZapCore\Logger;
-use BFITech\ZapStore as zs;
+require_once(__DIR__ . '/../vendor/bfitech/zapstore/tests/SQLite3Test.php');
 
-class SQLite3Test extends SQLTest {
-
-	public static $engine = 'sqlite3';
-
-	public function test_sqlite3() {
-		$logger = new Logger(
-			Logger::ERROR, getcwd() . '/zapstore-test.log');
-		$sql = new zs\SQLite3(['dbname' => ':memory:'], $logger);
-		$this->assertEquals(
-			$sql->get_connection_params()['dbtype'], 'sqlite3');
-	}
-
-}
