58512779873a90d409fb95e400bbbc17c30b91a6
Rendra Suroso <rs@cogsci.bandungfe.net>
2019-05-10 01:10:19 +0700

    Update zapstore to 2.0.0.

--- tests/SQLite3Test.php
+++ tests/SQLite3Test.php
@@ -1,9 +1,6 @@
 <?php
 
 
-use BFITech\ZapCommonDev\CommonDev;
-
-
-CommonDev::testdir(__FILE__);
-require_once(__DIR__ . '/../vendor/bfitech/zapstore/tests/SQLite3Test.php');
+require_once __DIR__ .
+	'/../vendor/bfitech/zapstore/tests/SQLite3Test.php';
 
